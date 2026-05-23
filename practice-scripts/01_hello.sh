07cf0b71a43e4a906eb48bf6227f2a272e575612
Shubham Londhe <shubhamnath5@gmail.com>
2026-05-24 03:34:48 +0530

    Change greeting from 'Namaste duniya!' to 'Hello Dosto!'

--- practice-scripts/01_hello.sh
+++ practice-scripts/01_hello.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # first script: echo prints to the terminal
 
-echo "Namaste duniya!"
+echo "Hello Dosto!"
 echo "Shell scripting shuru karte hain"
 echo "TrainWithShubham - Udaan batch 11"
