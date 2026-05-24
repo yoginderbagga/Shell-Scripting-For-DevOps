e598d284cd603a4a4c4a6a5c3a129afe7bffb512
Yoginder Bagga <yoginder.bagga@gmail.com>
2026-05-24 18:52:11 +0530

    Adding Second Shell Script : Variable

---
+++ day01/variable_1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#####################################################################
+#
+#	Name: Yoginder Bagga
+#	Purpose: using variable in shell
+#	Scope: For the learning
+#
+####################################################################
+
+## DECLARING VARIABLE, can hold any type of data
+
+name="Super Mario"
+score=50
+
+
+## ACCESSING VARIABLE, in the script 
+
+echo "Hello, $name!"
+echo "The total score is $score"
+
+
+
+
