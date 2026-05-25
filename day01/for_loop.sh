c9fcebd08ebf6fcb1ce22bfc54ed156f69d1a995
Yoginder Bagga <yoginder.bagga@gmail.com>
2026-05-25 18:49:02 +0530

    Adding scripts of for Loop in Bash

---
+++ day01/for_loop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+####################################################################
+#
+#	Name: Yoginder Bagga
+#	Purpose: Use read to take inputs from user and apply IF-ELSE condition
+#	Scope: For the learning
+#
+####################################################################
+
+
+echo -e "-----------------Script to Demonstrate FOR LOOP--------------\n"
+
+for colors in orange green yellow
+do
+	echo  "The color is : $colors"
+done
---
+++ day01/for_loop2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#####################################################################
+#
+#	Name: Yoginder Bagga
+#	Purpose: Using for loop to repeat a particular tasks
+#	Scope: For the learning
+#
+####################################################################
+
+echo -e "----------------Demonstrating For Loop with Range-------------------"
+
+for all_marks in {1..10}
+do	
+	echo "Your score is: $all_marks"
+done
